dd6e25602b53f94b8dac160f55a259eebd915d38
mrpeace12 <issahbest2022@gmail.com>
2026-01-08 13:26:29 +0000

    Test render App component

--- index.tsx
+++ index.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css"; // optional
-
-ReactDOM.createRoot(
-  document.getElementById("root")!
-).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function App() {
+  return (
+    <div style={{
+      padding: "40px",
+      fontSize: "24px",
+      background: "red",
+      color: "white"
+    }}>
+      React is working ✅
+    </div>
+  );
+}
